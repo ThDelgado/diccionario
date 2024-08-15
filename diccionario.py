2eb009ad3b9922923802bc67e3170657367d6050
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-08-15 09:32:40 -0400

    creacion de archivo

---
+++ diccionario.py
@@ -0,0 +1,34 @@
+# creacion de usuarios con diccionarios
+
+usuario1 = {
+    "Nombre" : "Juan",
+    "Apellido" : "Perez",
+    "Telefono" : "94561231"
+}
+
+usuario2 = {
+    "Nombre" : "Marta",
+    "Apellido" : "Sanchez",
+    "Telefono" : "456478977"
+}
+
+usuario3 = {
+    "Nombre": "Erica",
+    "Apellido" : "Pino",
+    "Telefono" : "123179789"
+}
+
+
+# creando lista y agregando elementos
+
+mylist = []
+
+mylist.append(usuario1)
+mylist.append(usuario2)
+mylist.append(usuario3)
+
+print ("contenido de mylist: ", mylist)
+
+# evaluacion m3s4
+# Thelma Delgado
+# Diccionario
